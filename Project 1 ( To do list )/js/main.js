ac2f748e1836e8f174030951d9a5a4f1fb26fd99
isayev89 <dev.isayev@gmail.com>
2022-06-13 22:17:01 +0400

    asd

---
+++ Project 1 ( To do list )/js/main.js
@@ -0,0 +1,11 @@
+let addToList = document.getElementById('addToList');
+
+addToList.addEventListener('click', event => {
+
+
+
+    // todo-list
+   let todoList = document.getElementsByClassName('todoList');
+   console.log(todoList);
+
+});
