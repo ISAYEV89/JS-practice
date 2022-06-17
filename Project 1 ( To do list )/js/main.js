5f11f50bd9d031f8ff16371f8ee353bc52e6d6e3
isayev89 <dev.isayev@gmail.com>
2022-06-17 23:55:28 +0400

    aaa

--- Project 1 ( To do list )/js/main.js
+++ Project 1 ( To do list )/js/main.js
@@ -2,11 +2,11 @@ let addToList = document.getElementById('addToList');
 
 addToList.addEventListener('click', event => {
 
-   let toDo = document.getElementById('listValue').value;
-   console.log(toDo);
+    let toDo = document.getElementById('listValue').value;
+    console.log(toDo);
 
-    // todo-list
-   let todoList = document.getElementById('todo-list');
-   console.log(todoList);
+
+    let todoList = document.getElementById('todo-list');
+
 
 });
